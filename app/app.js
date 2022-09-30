26a08f62a157669e344d9e718fde43cca8b27017
AminaAidarus <aaidaru1@my.centennialcollege.ca>
2022-09-30 16:57:58 -0400

    added some files and images and updated the abouts page

--- app/app.js
+++ app/app.js
@@ -26,7 +26,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname,'./public')));
+//app.use(express.static('public'));
+app.use('/public', express.static(__dirname + "/public"));
 app.use(session({
     secret: Secret,
     saveUninitialized: false,
